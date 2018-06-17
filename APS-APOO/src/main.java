481656e535c613b776284e47265380e53bd44143
= <=>
2018-06-17 00:47:42 -0300

    projeto reiniciado

--- APS-APOO/src/main.java
+++
@@ -1,8 +0,0 @@
-import view.ViewCadastro;
-
-public class main {
- public stativ void main(Strings []args) {
-    ViewCadastro viewCadastro = new ViewCadastro();
-    viewCadastro.setVisible(true);
- }
-}
