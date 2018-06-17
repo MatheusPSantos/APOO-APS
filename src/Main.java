e119063e35cbe89f15314cfbeb5634edf99c321b
= <=>
2018-06-17 00:57:30 -0300

    Main chamando a janela de cadastro

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import view.ViewCadastro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// exibindo a tela de cadastro inicialmente
+		ViewCadastro vc = new ViewCadastro();
+		vc.setVisible(true);
+
+		
+	}
+
+}
