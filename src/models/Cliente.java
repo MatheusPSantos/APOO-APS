b55872dbe956082b07d3f0da478b00c4af272cb3
= <=>
2018-06-19 23:17:55 -0300

    Classe Endereço Removida

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -9,7 +9,7 @@ package models;
 public class Cliente extends Usuario {
 
     private String cpf;
-    private Endereco endereco;
+
     // metodos
     public void setCPF(String cpf) {
         this.cpf = cpf;
@@ -17,50 +17,6 @@ public class Cliente extends Usuario {
 
     public String getCPF() {
         return this.cpf;
-    }
-    
-    public Endereco getEndereco() {
-    	return this.endereco;
-    }
-    
-    public void setEndereco(Endereco endereco) {
-    	this.endereco = endereco;
-    	
-    }
-    
-    // classe do endereco do cliente
-    public class Endereco {
-    	private String rua;
-    	private String bairro;
-    	private int numeroCasa;
-    	
-    	// metodo construtor
-    	public Endereco(String rua, String bairro, int numeroCasa) {
-    		this.rua = rua;
-    		this.bairro = bairro;
-    		this.numeroCasa = numeroCasa;
-    	}
-    	
-    	public void setRua(String rua) {
-    		this.rua = rua;
-    	}
-    	public String getRua() {
-    		return this.rua;
-    	}
-    	
-    	public void setBairro(String bairro) {
-    		this.bairro=bairro;
-    	}
-    	public String getBairro() {
-    		return this.bairro;
-    	}
-    	
-    	public void setNumeroCasa(int numero) {
-    		this.numeroCasa = numero;
-    	}
-    	public int getNumeroCasa() {
-    		return this.numeroCasa;
-    	}
     }
 }
 
