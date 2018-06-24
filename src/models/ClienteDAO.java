fa1c22f95c67a1f497008a55ebe5e9f85edfd2c8
= <=>
2018-06-24 14:55:57 -0300

    verificação de login

--- src/models/ClienteDAO.java
+++ src/models/ClienteDAO.java
@@ -2,6 +2,7 @@ package models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
@@ -38,4 +39,30 @@ public class ClienteDAO {
 			ConexaoMYSQL.closeConnection(con, stmt);
 		}
 	}
+	
+	// metodo de checagem do login
+	 public boolean checkLoginCliente(String cpf, String senha) {
+		 Connection con = ConexaoMYSQL.getConexaoMySQL();
+		 PreparedStatement stmt = null;
+		 ResultSet rs = null;
+		 boolean check = false;
+		 
+		 try {
+			 stmt = con.prepareStatement("SELECT * FROM cliente WHERE cpf = ? AND senha = ?");
+			 stmt.setString(1, cpf);
+			 stmt.setString(2, senha);
+			 
+			 rs = stmt.executeQuery();
+			 
+			 if(rs.next()) {
+				 check = true;				  
+			 }
+		 } catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao verificar login: "+e); 
+		 } finally {
+			 //fechando conexao
+			 ConexaoMYSQL.closeConnection(con, stmt, rs);
+		 }
+		 return check;
+	 }
 }
