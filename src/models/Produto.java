e5cc9da4bf053345ad18a12c63d9577954a7daa2
= <=>
2018-06-24 21:58:12 -0300

    atributos do produto criado

--- src/models/Produto.java
+++ src/models/Produto.java
@@ -6,5 +6,41 @@
 package models;
 
 public class Produto {
+    private String nome;
+    private Float valor;
+    private int quantidade;
+    private String marca;
+    private int prod_id;
     
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Float getValor() {
+		return valor;
+	}
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getProd_id() {
+		return prod_id;
+	}
+	public void setProd_id(int prod_id) {
+		this.prod_id = prod_id;
+	}
+        
 }
